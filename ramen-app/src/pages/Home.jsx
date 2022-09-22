02c9915bafbef2de21c9f0b2a29b09da10a31026
Timothy Tang <tangctimothy@gmail.com>
2022-09-21 22:50:15 -0400

    worked on the home page

--- ramen-app/src/pages/Home.jsx
+++ ramen-app/src/pages/Home.jsx
@@ -2,3 +2,20 @@ import Layout from "../components/Layout";
 import {Link} from 'react-router-dom'
 import {getRamen} from '../services/ramen'
 import './Home.css'
+import { useEffect } from "react";
+
+export default function Home ({user, toggle}) {
+    useEffect(() => {
+        const fetchRamen = async() => {
+            let response = await getRamen()
+            setRamen(response)
+        }
+    fetchRamen()
+          
+})
+return(
+    <Layout>
+    <h2>NYC Ramen</h2>
+</Layout>
+)
+}
